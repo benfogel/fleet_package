b8f39af5b9ac5050e9a052526bb70b3c382bbfef
Ben Fogel <bfogel@google.com>
2024-10-03 03:41:45 +0000

    update

--- build/deploy-fleet-package.sh
+++ build/deploy-fleet-package.sh
@@ -3,20 +3,30 @@ set +x
 
 export TAG=$1
 
-envsubst < fleet-package-dev1.yaml > fleet-package-target.yaml
+git tag $TAG
+git push origin --tags
 
-gcloud alpha container fleet packages describe fleet-package \
-    --project fp-demo-dev1 --location us-central1
+for f in *.yaml; do
 
-if [ $? -eq 0 ]; then
-    gcloud alpha container fleet packages update fleet-package \
-        --source=fleet-package-target.yaml \
-        --project=fp-demo-dev1
-else
-    gcloud alpha container fleet packages create fleet-package \
-        --source=fleet-package-target.yaml \
-        --project=fp-demo-dev1
-fi
+    envsubst < $f > "rendered-$f"
+
+    gcloud alpha container fleet packages describe fleet-package \
+        --location us-central1 \
+        #--project fp-demo-dev1
+
+    if [ $? -eq 0 ]; then
+        gcloud alpha container fleet packages update fleet-package \
+            --source="rendered-$f" \
+            # --project=fp-demo-dev1
+    else
+        gcloud alpha container fleet packages create fleet-package \
+            --source="rendered-$f" \
+            #--project=fp-demo-dev1
+    fi
+
+    rm "rendered-$f"
+done 
+
 
 
 
