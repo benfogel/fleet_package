afc82b4b7537f7fba697d41f0586ee1a46366a31
Ben Fogel <bfogel@google.com>
2024-10-03 04:08:35 +0000

    update

--- build/deploy-fleet-package.sh
+++ build/deploy-fleet-package.sh
@@ -9,20 +9,37 @@ git push origin --tags
 for f in *.yaml; do
 
     envsubst < $f > "rendered-$f"
+    PROJECT_RESOURCE=$(cat "rendered-$f" | yq '.target.fleet.project' -r) # returns format of projects/PROJECT_NAME
+    PROJECT=${PROJECT_RESOURCE#*/} # strips `projects/` prefix
 
     gcloud alpha container fleet packages describe fleet-package \
         --location us-central1 \
-        #--project fp-demo-dev1
+        --project=$PROJECT
 
     if [ $? -eq 0 ]; then
+        # Trigger new rollout
         gcloud alpha container fleet packages update fleet-package \
             --source="rendered-$f" \
-            # --project=fp-demo-dev1
+            --project=$PROJECT
     else
+        # First deployment of package
         gcloud alpha container fleet packages create fleet-package \
             --source="rendered-$f" \
-            #--project=fp-demo-dev1
+            --project=$PROJECT
     fi
+
+    # Poll for rollout to complete
+    STATE="PENDING"
+    RELEASE_NAME=$(echo $TAG | tr . -) # releases are named the same as the tag, but with `-` instead of `.`
+
+    while [[ "$STATE" != "COMPLETE" ]]; do
+        STATE=$(gcloud alpha container fleet packages rollouts list --fleet-package \
+            fleet-package --filter="release:$RELEASE_NAME" \
+            --project=$PROJECT \
+            --format yaml | yq '.info.state')
+
+        sleep 5
+    done
 
     rm "rendered-$f"
 done 
